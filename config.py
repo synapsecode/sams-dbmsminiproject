3604bae44e1fa1c10c69ae1649eafdbe5daff27e
Manas Hejmadi <manas2all@outlook.com>
2024-07-26 23:21:36 +0530

    vercel.json inclusion & cockroachdb migration

--- config.py
+++ config.py
@@ -3,5 +3,6 @@ import os
 class Config:
     SECRET_KEY = 'SECRETSECRETSECRET'
     # SQLALCHEMY_DATABASE_URI = 'postgresql://samsproject:@localhost/samsdatabase'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    SQLALCHEMY_DATABASE_URI = 'cockroachdb://krustel:vibe8FX9XSAchdkpZA9spQ@samsproject-5581.7s5.aws-ap-south-1.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
